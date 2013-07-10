3fd9b1289bd6291062b938c4a6f45dd4af7c34ca
Ubuntu <ubuntu@ip-172-31-6-14.us-west-1.compute.internal>
2013-07-10 15:13:33 +0000

    updated web.js to fix bugs

--- web.js
+++ web.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var fs = require('fs');
+var Buffer = require('buffer').Buffer;
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  // response.send('Hello World2!');
-	var buffer = new Buffer(100);
-	fs.readFileSync('index.html', function (err, data) {
+	//var buffer = new Buffer(100);
+	var buffer = fs.readFileSync('index.html', function (err, data) {
   		if (err) throw err;
   		buffer = data;
 	});
-	response.send(buffer);
+	response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
